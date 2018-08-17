10fdd7d9a6b48a9010b9dd8d7348426174ddea82
barema4 <samrubarema6@gmail.com>
2018-08-17 16:43:15 -0700

    Add urls page

---
+++ final/urls.py
@@ -0,0 +1,20 @@
+from spy_view import Questions, SpecificAnswer
+
+
+class Urls:
+
+    @staticmethod
+    def generate(app):
+
+        """
+        Generate urls on the app context
+        It takes no argument
+        :param: app: takes in the app variable
+        :return: urls
+        """
+
+        app.add_url_rule('/api/v1/questions/<int:id>', view_func=Questions.as_view('h'), methods=['GET', ])
+        app.add_url_rule('/api/v1/questions/', view_func=Questions.as_view('g'), methods=['GET', ])
+        app.add_url_rule('/api/v1/questions', view_func=Questions.as_view('w'), methods=['POST', ])
+        app.add_url_rule('/api/v1/questions/<int:id>/answers', view_func= SpecificAnswer.as_view('ansewr'), methods=['POST', ])
+
