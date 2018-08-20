ddd7361b34b705bb66952617cc5046f30d5d5f75
barema4 <samrubarema6@gmail.com>
2018-08-20 02:46:00 -0700

    Deleted app.py

--- apiwork/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-from urls import Urls
-APP = Flask(__name__)
-APP.env = 'development'
-APP.testing = True
-Urls.generate(APP)
-if __name__ == '__main__':
-    APP.run(debug=True)
